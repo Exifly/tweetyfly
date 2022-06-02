4c858bbc594ed05ac40c17d053de227837a622a2
gdjohn4s <john4s@protonmail.ch>
2022-06-02 21:09:24 +0200

    Fixed bug on check for bad words. Added bad words. Refactored some code.

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
+from re import I
 import requests
 import os
 import time
+import random
+import datetime
 from textblob import TextBlob
 from dotenv import load_dotenv
 from requests_oauthlib import OAuth1
@@ -18,7 +21,7 @@ ACCESS_SECRET = os.getenv("ACCESS_SECRET")
 
 
 def get_last_tweet_id_and_text():
-    # - Get desc sorted list of tweet id - #
+    # - Get desc sorted list of tweets text and id - #
     id_list = []
     tweets = {}
     tweet = []
@@ -39,7 +42,8 @@ def get_last_tweet_id_and_text():
     id_list = [id['id'] for id in resp['data']]
     for el in resp['data']:
         tweet.append({"id": el['id'], "text": el['text']})
-        tweets = {"tweets": tweet}
+
+    tweets = {"tweets": tweet}
 
     # print(tweets)
     return id_list, tweets
@@ -63,33 +67,80 @@ def retweet(id):
         f"https://api.twitter.com/2/users/{user_id}/retweets", auth=auth, json=payload).json()
 
     if (resp_2['data']['retweeted']):
-        print(f"Tweet N {id} succesfully retweeted.")
+        print(f"[{date()}] Tweet N {id} succesfully retweeted.")
     else:
-        print(f"Tweet N {id} not retweeted")
+        print(f"[{date()}] Tweet N {id} not retweeted")
 
 
 def analyze_text(tweets):
-    bad_words = ["pippo", "pluto", "topolino", "gesuel", "bastard"]
+    bad_words = ["course", "crash course", "ads",
+                 "cash", "sponsor", "freelancer", "pay", "payd", "price", "USD",
+                 "client:", "freelance", "freelancer", "assistance", "homework",
+                 "exam", "exams", "pay", "us", "job", "free", "need", "helping",
+                 "we", "do", "help", "stuck", "homework", "bot", "crypto", "finance", "budget",
+                 "class", "classes", "room"]
+
     checked_id = 0
+    counter = 0
 
     for text in tweets['tweets']:
-        print(text)
+        print(f"[{date()}] Checked Tweet N {counter}")
         blob = TextBlob(text=text['text'])
-        print(blob.words)
-        if (blob.words in bad_words):
-            continue
-        else:
-            checked_id = text['id']
-            print(f"Checked ID -->> {checked_id}")
-            return checked_id
+        print(f"[{text['id']}] {blob.words.lower()}")
+        blob_words = blob.words.lower()
+
+        count_list = 0
+        blob_len = len(blob_words)
+        for x in blob_words:
+            if x not in bad_words:
+                count_list += 1
+                if count_list == blob_len:
+                    print(f"[{date()}] Oh, that's a good one!")
+                    checked_id = text['id']
+                    print(f"[{date()}] Checked ID -->> {checked_id}")
+                    return checked_id
+            else:
+                print(f"[{date()}] Tweet with id {text['id']} is not good..")
+                counter += 1
+                break
 
 
-print("Bot Start")
+def like(id):
+    body = {
+        "tweet_id": id
+    }
+
+    auth = OAuth1(
+        client_key=API_KEY,
+        client_secret=API_SECRET,
+        resource_owner_key=ACCESS_TOKEN,
+        resource_owner_secret=ACCESS_SECRET
+    )
+
+    user_id = os.getenv("MY_USER_ID")
+    resp = requests.post(
+        f"https://api.twitter.com/2/users/{user_id}/likes", auth=auth, json=body)
+    if (resp.status_code == 200):
+        resp_parsed = resp.json()
+        print(f"[{date()}] Oooohhh, Tweet {id} Liked!!\n", resp_parsed)
+        return
+    else:
+        print(f"[{date()}] Error during like request API!")
+
+
+def date(): return datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+
+print(f"[{date()}] Bot Start")
+
 while True:
+    delay = random.randint(60, 180)
+    print(f"[{date()}] Delay: {delay}")
     tweet_id_list = []
     tweets_list = {}
     tweet_id_list, tweets_list = get_last_tweet_id_and_text()
     checked_id = analyze_text(tweets=tweets_list)
-    print(checked_id)
-    # retweet(tweet_id_list[0])
-    time.sleep(5)
+    like(tweet_id_list[0])
+    retweet(tweet_id_list[0])
+    print(" ")
+    time.sleep(delay)
